a07e937c64297cf7652d34c5fdd12313759ca25f
Sergio Ramos <sergioramos_92@hotmail.com>
2023-08-19 20:49:09 -0300

    se agregan algunas soluciones

--- logico/functores-polimorfismo.pl
+++ logico/functores-polimorfismo.pl
@@ -1,5 +1,6 @@
 % FUNCTORES Y POLIMORFISMO
-% vende(Titulo, Auto, Genero, Editorial, Precio).
+% supongamos que tenemos el siguiente predicado
+% vende(Titulo, Autor, Genero, Editorial, Precio).
 
 % FUNCTORES
 % sería más o menos como tener un predicado asociado a otro, o que se puede usar un predicado
@@ -13,7 +14,7 @@ Articulo = libro(Nombre, Autor, Genero, Editorial) | cd(Titulo, Autor, Genero, C
 % nos podemos permitir hacer lo siguiente
 vende(libro(harryPotter1, jkRowling, fantasia, salamandra), 1500).
 vende(cd(saturate, breakingBenjamin, rock, 10, 13), 2000).
-% libro/4 y cd/4 son functores
+% libro/4 y cd/5 son functores
 
 % los functores solo pueden estar como parámetros de los predicados
 
@@ -28,7 +29,7 @@ vende(cd(saturate, breakingBenjamin, rock, 10, 13), 2000).
 % si todo lo que se vende es de él
 
 tematico(Autor) :-
-    forall(vende(Articulo, _), autor(Articulo, Auto)).
+    forall(vende(Articulo, _), autor(Articulo, Autor)).
 
 % pero autor/2 no existe como predicado, entonces lo creamos
 autor(libro(_, Autor, _, _), Autor) :-
@@ -39,4 +40,81 @@ autor(cd(_, Autor, _, _, _), Autor) :-
 % estamos aplicando polimorfismo en autor/2 (es un predicado polimorfico)
 % Articulo puede tener forma de libro o cd, pero no nos interesa
 
-% 32:30
+% un poco más de teoría de la wiki de uqbar
+% nos dice que los functores nos permiten agrupar individuos en grupos para formar algo más abstracto
+% por ejemplo, si tenemos Artículos, podemos tener cd's y libros, pero simplemente los agrupamos en
+% Artículos, porque es lo que son, Artículos
+
+% estos functores tienen un nombre y una aridad determinada, por ejemplo, para nuestro caso tenemos
+% cd de aridad 4 y libro de aridad 5
+% podemos tener lo siguiente
+habitat(Animal, Bioma).
+
+mamifero(Nombre, CantPatas, Alimentacion).
+reptil(Nombre, CantPatas, Alimentacion).
+
+habitat(mamifero(tigre, 4, carnivoro), sabana).
+habitat(reptil(cocodrilo, 4, carnivoro), pantano).
+
+% lo que hay que resaltar es que no son predicados, son valores, osea, no tienen
+% un valor de verdad (True o False)
+% no podemos usarlos de raíz, es decir, fuera de un predicado
+% tiene que estar dentro de un predicado o (creo que se puede), dentro
+% de otro functor
+
+
+
+% Practica
+
+% nuestra BASE DE CONOCIMIENTO
+% vende/2 (Articulo y Precio)
+% autor/2 (Articulo y Autor)
+
+% Extender la base de conocimiento de los siguientes predicados
+% 1. libroMásCaro/1: Se cumple para un artículo si es el libro de mayor precio.
+libroMasCaro(Articulo) :-
+    not((vende(Articulo, Precio), vende(OtroArticulo, OtroPrecio), OtroPrecio > Precio)).
+% esta no es una solución (está mal)
+
+% el predicado se usará solo para Libros
+% entonces podemos añadir el functor dentro del predicado
+libroMasCaro(libro(Titulo, Autor, Genero, Editorial)) :-
+    vende(libro(Titulo, Autor, Genero, Editorial), Precio),
+    forall(vende(libro(_, _, _, _), OtroPrecio), OtroPrecio =< Precio).
+% debemos ligar primero todos los libros que se vende a
+% un determinado precio
+% luego no aseguramos que para el libro en cuestión no haya ningún otro que
+% sea más caro
+
+% 2. curiosidad/1: Se cumple para un artículo si es lo único que hay
+% a la venta de su autor.
+curiosidad(Articulo) :-
+    autor(Articulo, Autor),
+    forall(autor(OtroArticulo, OtroAutor), Autor = OtroAutor).
+
+curiosidad(Articulo) :-
+    vende(Articulo, _),
+    autor(Articulo, Autor),
+    not((vende(OtroArticulo, _), autor(OtroArticulo, Autor), Articulo \= OtroArticulo)).
+% solución propuesta en el video
+
+
+% 42:42
+    
+
+% 3. sePrestaAConfusión/1: Se cumple para un título si pertenece a
+% más de un artículo.
+sePrestaALaConfusion(Titulo) :-
+    vende(libro(Titulo, _, _, _), _),
+    vende(cd(OtroTitulo, _, _, _, _), _),
+    Titulo = OtroTitulo.
+
+% 4. mixto/1: Se cumple para los autores de más de un tipo de articulo.
+mixto(Autor) :-
+    vende(libro(_, Autor, _, _), _),
+    vende(cd(_, OtroAutor, _, _, _), _),
+    Autor = OtroAutor.
+
+% 5. Agregar soporte para vender Películas con título
+% director y género.
+pelicula(Titulo, Director, Genero).
